bf6e5a71ea3d9cc6694e6f0d44935f2ab4d385dc
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-02-04 20:37:12 +0500

    Updated App.js file with outlet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
@@ -9,7 +11,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Proshop</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
