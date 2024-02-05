31fc5efa26c6f131e6702131f005b04448b941cc
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-02-05 14:06:29 +0500

    setup frontend and backend run concurrently and add essiential dev dep

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 
 import products from "./data/products.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-const port = 5000;
+const port = process.env.PORT || 8000;
 
 const app = express();
 
