a34aa0fd9582a40d9f4888d45169e8bfd0f6f9bd
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-03-18 22:20:20 +0500

    Models for BD

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+import mongooes from "mongoose";
+
+const userSchema = new mongooes.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongooes.model("User", userSchema);
+export default User;
