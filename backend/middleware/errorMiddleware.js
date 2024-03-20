9930fe5e0e04906eb2507b174f43c4e749edbb9a
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-03-21 04:57:32 +0500

    Handle the errors

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,22 @@
+const notFount = (req, res, next) => {
+  let error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  // check for Mongooes bad objectID
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    message = "Resource not found";
+    statusCode = 404;
+  }
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? "🍕" : err.stack,
+  });
+};
+
+export { notFount, errorHandler };
