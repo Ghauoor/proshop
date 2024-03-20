6bff121c7ecc30e5763220ddedd8caf171d765c1
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-03-21 04:58:15 +0500

    routes and middlewares setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
-
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/db.js";
+import { notFount, errorHandler } from "./middleware/errorMiddleware.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,18 +10,9 @@ const port = process.env.PORT || 8000;
 connectDB();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Api is running....");
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-
-  res.json(product);
-});
+app.use(notFount);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
