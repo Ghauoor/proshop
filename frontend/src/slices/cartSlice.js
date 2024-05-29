883aff4768fa3b4f143718803d4822e5a9a05d22
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-05-30 01:13:16 +0500

    Add Shopping data in cart slice

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [] };
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
 
 const cartSlice = createSlice({
   name: "cart",
@@ -34,9 +34,14 @@ const cartSlice = createSlice({
       localStorage.removeItem("cart");
       return updateCart(state);
     },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
+    },
   },
 });
 
-export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, clearCart, saveShippingAddress } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
